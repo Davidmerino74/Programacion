d4ecb5ab5fd56d1772c302f1374655a43ff35064
JonAnderAsua <jonanderasua@gmail.com>
2024-08-01 20:39:28 +0200

    Add la parte de soluciones

---
+++ src/Practica1_sol/EjerciciosVariablesSol.java
@@ -0,0 +1,125 @@
+package Practica1;
+
+public class EjerciciosVariables {
+     // ESTE MÉTODO NI TOCAR
+    public static void main(String[] args) throws Exception {
+        // Si no queréis que se ejecute un método (ejercicio) en concreto comentadlo usando la barra de comentar --> // 
+        ejercicio1();
+    }
+
+    // A partir de este punto empiezan los ejercicios, los nombres de cada método indican a que ejercicio pertenecen
+    // Dadle caña y no dudeis en preguntar vuestras dudas :)
+
+    private static void ejercicio1(){
+        /*
+            Programa java que realice lo siguiente: declarar una variable N de tipo int, 
+            una variable A de tipo double y una variable C de tipo char y asigna a cada una un valor. 
+            A continuación muestra por pantalla: El valor de cada variable, la suma de N + A, 
+            la diferencia de A - N, el valor numérico correspondiente al carácter que contiene la variable C.
+       */ 
+        System.out.println("Ejercicio número 1");
+
+        // Vuestro código va aquí
+
+
+        //Final del ejercicio
+        System.out.println("Final del ejercicio número 1");
+
+    }
+
+    private static void ejercicio2(){
+        /* 
+            Programa java que realice lo siguiente: declarar dos variables X e Y de tipo int, 
+            dos variables N y M de tipo double y asigna a cada una un valor. A continuación reliza 
+            y muestra por pantalla una serie de operaciones entre ellas.
+        */
+
+        System.out.println("Ejercicio número 2");
+
+        // Vuestro código va aquí
+
+
+        //Final del ejercicio
+        System.out.println("Final del ejercicio número 2");
+    }
+
+    private static void ejercicio3(){
+        /*
+            Programa Java que declare una variable entera N y asígnale un valor. A continuación escribe 
+            las instrucciones que realicen lo siguiente: Incrementar N en 77, Decrementarla en 3, Duplicar su valor.
+        */
+
+        System.out.println("Ejercicio número 3");
+
+        // Vuestro código va aquí
+
+
+        //Final del ejercicio
+        System.out.println("Final del ejercicio número 3");
+    }
+
+    private static void ejercicio4(){
+        /*
+            Programa java que declare cuatro variables enteras A, B, C y D y asígnale un valor acada una.
+            A continuación realiza las instrucciones necesarias para que: B tome el valor de C, C tome el valor de A, 
+            A tome el valor de D, D tome el valor de B. 
+        */
+
+        System.out.println("Ejercicio número 4");
+
+        // Vuestro código va aquí
+
+
+        //Final del ejercicio
+        System.out.println("Final del ejercicio número 4");
+    }
+
+    private static void ejercicio5(){
+        /*
+            Programa Java que declare una variable A de tipo entero y asígnale un valor. A continuación muestra 
+            un mensaje indicando si A es par o impar. Utiliza el operador condicional ( ? : ) dentro del println 
+            para resolverlo.
+        */
+
+        System.out.println("Ejercicio número 5");
+
+        // Vuestro código va aquí
+
+
+        //Final del ejercicio
+        System.out.println("Final del ejercicio número 5");
+    }
+
+    private static void ejercicio6(){
+        /* 
+            Programa Java que declare una variable B de tipo entero y asígnale un valor. A continuación muestra un 
+            mensaje indicando si el valor de B es positivo o negativo. Consideraremos el 0 como positivo. 
+            Utiliza el operador condicional (? : ) dentro del println para resolverlo.
+        */
+
+        System.out.println("Ejercicio número 6");
+
+        // Vuestro código va aquí
+
+
+        //Final del ejercicio
+        System.out.println("Final del ejercicio número 6");
+    }
+
+    private static void ejercicio7(){
+        /*
+            Programa Java que declare una variable C de tipo entero y asígnale un valor. A continuación muestra un 
+            mensaje indicando si el valor de C es positivo o negativo, si es par o impar, si es múltiplo de 5, si 
+            es múltiplo de 10 y si es mayor o menor que 100. Consideraremos el 0 como positivo. Utiliza el operador 
+            condicional ( ? : ) dentro del println para resolverlo. 
+        */
+
+        System.out.println("Ejercicio número 7");
+
+        // Vuestro código va aquí
+
+
+        //Final del ejercicio
+        System.out.println("Final del ejercicio número 7");
+    }
+}
