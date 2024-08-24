019134bb7c9a4ff2f6b605b125c18bb0ec2fb51e
JonAnderAsua <jonanderasua@gmail.com>
2024-08-24 18:32:58 +0200

    Empezando a poner cosas de la práctica 2

---
+++ src/Practica2/Libro.java
@@ -0,0 +1,15 @@
+package Practica2;
+
+public class Libro {
+
+    //Aquí tenéis que crear el método constructor con los siguientes atributos:
+    /*
+     * titulo: El título del libro en formato String.
+     * fecha_publ: El año de publicación del libro en formato Integer.
+     * autor: El nombre y el apellido del autor o autora del libro en formato String
+     * 
+     */
+    
+
+    //Getters y setters
+}
